06f3bb4f26e4894744e8e526ea0bed55249ab64f
Fabian Lema <fabianlemac@gmail.com>
2019-07-19 10:37:54 -0500

    GetItems function

--- scripts/api.js
+++ scripts/api.js
@@ -6,7 +6,13 @@ const api = function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jessicafabian';
 
   const getItems =  function () {
-    return Promise.resolve('A successful response!');
+  	return fetch(BASE_URL + "/items")
+  		.then(res => {
+  			//console.log(res.json());
+  			return res.json();
+  		})
+  		.catch(err => console.error(err.message));
+    //return Promise.resolve('A successful response!');
   };
 
 
