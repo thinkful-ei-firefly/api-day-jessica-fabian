8cb0b0565910bcbdae7d373f81355b8cc5a1b8e8
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-19 11:58:32 -0400

    add Pt3

--- scripts/api.js
+++ scripts/api.js
@@ -6,20 +6,27 @@ const api = function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jessicafabian';
 
   const getItems =  function () {
-  	return fetch(BASE_URL + "/items")
-  		.then(res => {
-  			//console.log(res.json());
-  			return res.json();
-  		})
+  	return fetch(`${BASE_URL}/items`)
   		.catch(err => console.error(err.message));
     //return Promise.resolve('A successful response!');
   };
 
+  const createItem = function (name) {
+    const newItem = JSON.stringify({ name, });
+    return fetch(`${BASE_URL}/items`, {
+      method : 'POST', 
+      headers : {'Content-Type' : 'application/json'}, 
+      body : newItem});
+      
+
+  };
 
 
-  
+
+
   return {
     getItems,
+    createItem,
   };
 
 }();
--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,14 @@ $(document).ready(function() {
 
 store.items.push(Item.create('apples'));
 
-api.getItems()
-  .then(res => console.log(res));
-console.log(api.BASE_URL);
+/*
+api.createItem('pears')
+  .then(res => res.json())
+  .then(newItem => {
+    return api.getItems();
+  })
+  .then(res => res.json())
+  .then((items) => {
+    console.log(items);
+  });
+  */
